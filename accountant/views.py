ed7531c4d355dbf0a701431bd9048f48e7fbfe0f
stillnurs <noorsultan.mamataliev@gmail.com>
2020-12-09 14:01:12 +0600

    fixed accountant view

--- accountant/views.py
+++ accountant/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
 
 from .serializers import *
 
@@ -8,13 +7,16 @@ from .serializers import *
 class DailySalesReportView(generics.ListCreateAPIView):
     queryset = DailySalesReport.objects.all()
     serializer_class = DailySalesReportSerializers
+    permission_classes = (AllowAny,)
 
 
 class WeeklySalesReportView(generics.ListCreateAPIView):
     queryset = DailySalesReport.objects.all()
     serializer_class = WeeklySalesReportSerializers
+    permission_classes = (AllowAny,)
 
 
 class MonthlySalesReportView(generics.ListCreateAPIView):
     queryset = DailySalesReport.objects.all()
     serializer_class = MonthlySalesReportSerializers
+    permission_classes = (AllowAny,)
