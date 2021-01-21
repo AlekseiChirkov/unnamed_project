e7a37674c94d076e5501c59cb2c00a2cb753ef03
Scareface <tektonikboy98@gmail.com>
2021-01-21 13:07:53 +0600

    Fixed tables

--- reports/serializers.py
+++ reports/serializers.py
@@ -9,7 +9,6 @@ class ExcelFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExcelFile
         fields = '__all__'
-        # fields = ['excel_file']
 
 
 class ClothingSizeSerializer(serializers.ModelSerializer):
@@ -28,14 +27,3 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = '__all__'
-
-
-class ReportReadableSerializer(serializers.ModelSerializer):
-    article = ArticleSerializer()
-    clothing_size = ClothingSizeSerializer()
-    excel_file = ExcelFileSerializer()
-
-    class Meta:
-        model = Report
-        fields = '__all__'
-
--- reports/views.py
+++ reports/views.py
@@ -4,12 +4,12 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from reports.serializers import (
-    ReportSerializer, ReportReadableSerializer, ArticleSerializer, ClothingSizeSerializer, ExcelFileSerializer
+    ReportSerializer, ArticleSerializer, ClothingSizeSerializer, ExcelFileSerializer
 )
 from reports.models import (
     Report, Article, ClothingSize, ExcelFile
 )
-from reports.utils import excel_data_in_model
+# from reports.utils import excel_data_in_model
 
 
 class ExcelFileViewSet(ModelViewSet):
@@ -18,10 +18,10 @@ class ExcelFileViewSet(ModelViewSet):
     serializer_class = ExcelFileSerializer
 
     def create(self, request, *args, **kwargs):
-        excel_data_in_model(request)
+        # excel_data_in_model(request)
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid()
-        serializer.save()
+        serializer.save(user=self.request.user)
         return Response({
             'success': "File loaded successfully.",
         })
@@ -29,22 +29,22 @@ class ExcelFileViewSet(ModelViewSet):
 
 class ClothingSizeViewSet(ModelViewSet):
     # permission_classes = (IsAuthenticated, )
-    queryset = ClothingSize.objects.filter()
+    queryset = ClothingSize.objects.all()
     serializer_class = ClothingSizeSerializer
 
 
 class ArticleViewSet(ModelViewSet):
     # permission_classes = (IsAuthenticated, )
-    queryset = Article.objects.filter()
+    queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
 class ReportViewSet(ModelViewSet):
     # permission_classes = (IsAuthenticated, )
-    queryset = Report.objects.filter()
+    queryset = Report.objects.all()
     serializer_class = ReportSerializer
 
     def list(self, request, *args, **kwargs):
-        news = self.queryset.all()
-        serializer = ReportReadableSerializer(news, many=True)
+        report = self.queryset.all()
+        serializer = self.serializer_class(report, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
